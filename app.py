8d999f4b4915e061ca45d1d3a36f931739091237
Mason Daugherty <mdaugher@bowdoin.edu>
2024-11-28 01:09:04 -0600

    Add message TTL to dead letter queue declaration with error handling

--- app.py
+++ app.py
@@ -85,7 +85,25 @@ class RabbitMQBaseConsumer:
         self.channel.exchange_declare(
             exchange="dead_letter_exchange", exchange_type="direct"
         )
-        self.channel.queue_declare(queue="dead_letter_queue", durable=True)
+
+        try:
+            self.channel.queue_declare(
+                queue="dead_letter_queue",
+                durable=True,
+                arguments={
+                    "x-message-ttl": 60000,
+                },
+            )
+        except pika.exceptions.ChannelClosedByBroker:
+            logger.warning(
+                "Queue 'dead_letter_queue' already exists with different attributes."
+                "Skipping re-declaration."
+            )
+            self.connection = pika.BlockingConnection(
+                self.connection.connection_parameters
+            )
+            self.channel = self.connection.channel()
+
         self.channel.queue_bind(
             exchange="dead_letter_exchange", queue="dead_letter_queue"
         )
