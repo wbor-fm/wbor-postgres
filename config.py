5b8a1123035f29c3ed2db72045d115a7a59ca4be
Mason Daugherty <mdaugher@bowdoin.edu>
2024-12-08 04:28:39 -0500

    Update RabbitMQ host configuration in config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 APP_PORT = os.getenv("APP_PORT", "3000")
 
-RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "wbor-rabbitmq")
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
 RABBITMQ_USER = os.getenv("RABBITMQ_USER", "guest")
 RABBITMQ_PASS = os.getenv("RABBITMQ_PASS", "guest")
 RABBITMQ_EXCHANGE = os.getenv("RABBITMQ_EXCHANGE", "source_exchange")
